af542184640bd1446d4c8ca7fdfbf9e4056856d7
hburaks <hasanburaksongur@gmail.com>
2023-06-13 06:37:28 +0300

    Create Recursive Power project

---
+++ src/RecursivePower.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class RecursivePower {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Taban değerini giriniz: ");
+        int base = scanner.nextInt();
+
+        System.out.print("Üs değerini giriniz: ");
+        int exponent = scanner.nextInt();
+
+        int sonuc = power(base, exponent);
+        System.out.println("Sonuç: " + sonuc);
+    }
+
+    public static int power(int base, int exponent) {
+        if (exponent == 0) {
+            return 1;
+        } else {
+            return base * power(base, exponent - 1);
+        }
+    }
+}
