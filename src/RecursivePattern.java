0c653b89aeceeeefa2717f4f0e5302b826434a64
hburaks <hasanburaksongur@gmail.com>
2023-06-13 07:44:10 +0300

    Create Recursive Pattern project

---
+++ src/RecursivePattern.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class RecursivePattern {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = inp.nextInt();
+        System.out.println("N number: " + number);
+        System.out.print("Pattern: ");
+        pattern(number);
+    }
+
+    public static void pattern(int number) {
+        System.out.print(number + " ");
+        if(number <= 0){
+            return;
+        }
+        pattern(number -5);
+        System.out.print(number + " ");
+
+
+    }
+}
