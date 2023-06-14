c91d7dbeda6aafd71bd9403b18acbddc26c7390c
hburaks <hasanburaksongur@gmail.com>
2023-06-14 14:54:09 +0300

    Create isDuplicate method

--- src/ArrayHelper.java
+++ src/ArrayHelper.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 public class ArrayHelper {
     public static double average(int[] arr){
         double sum = 0.0;
@@ -36,12 +37,37 @@ public class ArrayHelper {
             }
             System.out.println();
         }
+        System.out.println();
+
+    }
+    public static int[] isDuplicate(int[] arr){
+        int[] duplicated = new int[arr.length];
+        int count = 0;
+        for(int i = 0; i < arr.length; i++){
+            boolean isDuplicate = false;
+            for(int j = i + 1; j<arr.length; j++){
+                if(arr[j] == arr[i]){
+                    isDuplicate = true;
+                    break;
+                }
+            }
+            if(isDuplicate){
+                duplicated[count] = arr[i];
+                count++;
+            }
+
+        }
+        int[] result = new int[count];
+        System.arraycopy(duplicated, 0, result, 0, count);
+        return result;
     }
     public static void main(String[] args) {
-        int[] list = {14, 18, 23, 500, 15};
+        int[] list = {14, 18, 23, 500, 15,15};
         System.out.println(ArrayHelper.average(list));
         System.out.println(ArrayHelper.minMax(list));
         printB();
+        int[] duplicates = ArrayHelper.isDuplicate(list);
+        System.out.println(Arrays.toString(duplicates));
     }
 
 }
