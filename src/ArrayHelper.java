8cbf04287e6d437ca057d832da5f15c09212ed58
hburaks <hasanburaksongur@gmail.com>
2023-06-14 14:19:10 +0300

    Create Max and Min method

--- src/ArrayHelper.java
+++ src/ArrayHelper.java
@@ -7,9 +7,19 @@ public class ArrayHelper {
 
         return sum/arr.length;
     }
+    public static String minMax(int[] arr){
+        int max = arr[0];
+        int min = arr[0];
+        for(int i : arr){
+            max = Math.max(max, i);
+            min = Math.min(min, i);
+        }
+        return max + " " + min;
+    }
     public static void main(String[] args) {
         int[] list = {14, 18, 23, 500, 15};
         System.out.println(ArrayHelper.average(list));
+        System.out.println(ArrayHelper.minMax(list));
     }
 
 }
