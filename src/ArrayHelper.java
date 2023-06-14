cd3193dbf843691019d9816072a3f09d9cade5b3
hburaks <hasanburaksongur@gmail.com>
2023-06-14 14:27:30 +0300

    Create printB method

--- src/ArrayHelper.java
+++ src/ArrayHelper.java
@@ -16,10 +16,32 @@ public class ArrayHelper {
         }
         return max + " " + min;
     }
+    public static void printB(){
+        String[][] letter = new String[5][4];
+        for (int i = 0; i < letter.length; i++) {
+            for (int j = 0; j < letter[i].length; j++) {
+                if (i == 0 || i == 2 || i == 4) {
+                    letter[i][j] = " * ";
+                } else if (j == 0 || j == 3) {
+                    letter[i][j] = " * ";
+                } else {
+                    letter[i][j] = "   ";
+                }
+            }
+        }
+        System.out.println();
+        for (String[] row : letter){
+            for (String col : row){
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         int[] list = {14, 18, 23, 500, 15};
         System.out.println(ArrayHelper.average(list));
         System.out.println(ArrayHelper.minMax(list));
+        printB();
     }
 
 }
