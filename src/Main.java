f467ce4fe24ac26adc621b6536d68d31256e68de
H.Burak <123594944+hburaks@users.noreply.github.com>
2023-06-17 21:51:22 +0300

    Create Dynamic Array and it's specified methods

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        DynamicArray<Integer> dynamicArray = new DynamicArray<>();
+        dynamicArray.add(10);
+        dynamicArray.add(20);
+        dynamicArray.add(30);
+
+        System.out.println("Eleman sayısı: " + dynamicArray.size());
+
+        for (int i = 0; i < dynamicArray.size(); i++) {
+            System.out.println("Index " + i + ": " + dynamicArray.get(i));
+        }
+        System.out.println(dynamicArray.getCapacity());
     }
 }
